33704702e89c81c15f3e02a26dd0e1f38d56e6cc
patrykblajer <patryk@blajer.dev>
2023-06-11 13:07:43 +0200

    added new challenges

---
+++ src/codewars/CodelandUsernameValidation.java
@@ -0,0 +1,16 @@
+package codewars;
+
+class CodelandUsernameValidation {
+    public static void main(String[] args) {
+        codelandUsernameValidation("test");
+    }
+
+    public static String codelandUsernameValidation(String str) {
+
+        if (str.matches("^(?!.*_+$)[A-Za-z][A-Za-z0-9_]{2,24}$")) {
+            return "true";
+        }
+
+        return "false";
+    }
+}
---
+++ src/codewars/FirstFactorial.java
@@ -0,0 +1,21 @@
+package codewars;
+
+class FirstFactorial {
+
+    public static int firstFactorial(int num) {
+
+        int result = 1;
+
+        for (int i = num; i > 0; i--) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstFactorial(4));
+        System.out.println(firstFactorial(8));
+    }
+
+}
